78028cadefca0382de5a2b07d244bdc026dd185a
Mochamad Dicky <m_ardiansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 13:43:55 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,45 @@
+//Object Class
+public class Employee {
+    private String nama;
+    private double gaji;
+
+	public String getNama() {
+		return this.nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public double getGaji() {
+		return this.gaji;
+	}
+
+	public void setGaji(double gaji) {
+		this.gaji = gaji;
+	}
+
+
+    /*Constructor nama sama dengan nama class */
+    public Employee(){
+        nama = "";
+        gaji = 0.00;
+    }
+    
+    //Constructor parameter
+    public Employee (String nama , double gaji) {
+        this.nama = nama;
+        this.gaji= gaji;
+    }
+
+
+    //Method
+    public void print() {
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("Ini Data Dari Karyawan");
+        System.out.println("Nama : "+ nama);
+        System.out.println("Gaji : "+ gaji);
+        System.out.println("-------------------------------------------------------------------------");
+    }
+
+}
